620f1a44b3fc2c1300e880024465b12484a0f637
cse30fa22il <kegutman@ucsd.edu>
2024-02-08 09:59:27 -0800

    created GoalListFragment

---
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/goallist/GoalListFragment.java
@@ -0,0 +1,55 @@
+package edu.ucsd.cse110.successorator.ui.goallist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import edu.ucsd.cse110.successorator.R;
+import edu.ucsd.cse110.successorator.databinding.FragmentGoalListBinding;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GoalListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GoalListFragment extends Fragment {
+
+    private FragmentGoalListBinding view;
+
+    public GoalListFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static GoalListFragment newInstance() {
+        GoalListFragment fragment = new GoalListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+       // return inflater.inflate(R.layout.fragment_goal_list, container, false);
+        this.view = FragmentGoalListBinding.inflate(inflater, container, false);
+        return view.getRoot();
+    }
+
+}
