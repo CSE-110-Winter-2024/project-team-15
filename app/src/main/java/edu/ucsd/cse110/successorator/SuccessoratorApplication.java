ebbdd9e8e251e10dd690ada82bfdbd3530c364f8
Yoav Gutman <ygutman@ucsd.edu>
2024-02-08 08:46:23 -0800

    wrote rudimentary Application from lab4

--- app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
@@ -1,7 +1,21 @@
 package edu.ucsd.cse110.successorator;
 
 import android.app.Application;
+import edu.ucsd.cse110.successorator.lib.data.InMemoryDataSource;
+import edu.ucsd.cse110.successorator.lib.domain.GoalRepository;
 
+// default implementation from lab 4 -- uses default goals
 public class SuccessoratorApplication extends Application {
-
+    private InMemoryDataSource dataSource;
+    private GoalRepository goalRepository;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        // we use the default goals here -- change later
+        this.dataSource = InMemoryDataSource.fromDefault();
+        this.goalRepository = new GoalRepository(dataSource);
+    }
+    public GoalRepository getGoalRepository() {
+        return goalRepository;
+    }
 }
