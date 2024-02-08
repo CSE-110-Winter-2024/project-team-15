0c78021af37a3e1af4ab0994226dce88b8d7b9f7
Yoav Gutman <ygutman@ucsd.edu>
2024-02-08 07:51:51 -0800

    added id to Goal.java

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
@@ -1,21 +1,21 @@
 package edu.ucsd.cse110.successorator.lib.domain;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class Goal {
     // we do not want null strings -- that is lame
     private final @NonNull String contents;
-    public Goal(@NonNull String contents){
+    private final @Nullable Integer id;
+    public Goal(@NonNull String contents,
+                @Nullable Integer id){
         this.contents = contents;
+        this.id = id;
     }
     public @NonNull String contents(){
         return this.contents;
     }
-    /*
-     * make new goals with this static factory method
-     * (can change if needed)
-     */
-    public static Goal withContents(@NonNull String contents){
-        return new Goal(contents);
+    public @Nullable Integer id(){
+        return this.id;
     }
 }
