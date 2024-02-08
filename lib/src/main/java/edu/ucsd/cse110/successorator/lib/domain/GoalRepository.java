27d1b5f96661d19a715b34482b981f8b1c436154
Yoav Gutman <ygutman@ucsd.edu>
2024-02-08 08:22:25 -0800

    finished initial GoalRepository.java

---
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/GoalRepository.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import java.util.List;
+
+import edu.ucsd.cse110.successorator.lib.data.InMemoryDataSource;
+import edu.ucsd.cse110.successorator.lib.util.MutableSubject;
+
+public class GoalRepository {
+    private final InMemoryDataSource dataSource;
+    public GoalRepository(InMemoryDataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    public Integer count() {
+        return dataSource.getGoals().size();
+    }
+
+    public MutableSubject<Goal> find(int id) {
+        return dataSource.getGoalSubject(id);
+    }
+
+    public MutableSubject<List<Goal>> findAll() {
+        return dataSource.getAllGoalsSubject();
+    }
+
+    public void save(Goal goal) {
+        dataSource.putGoal(goal);
+    }
+}
