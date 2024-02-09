7ea0a8939fab3c9788b508bd5de992d4d373556b
cse30fa22il <kegutman@ucsd.edu>
2024-02-09 07:21:55 -0800

    mainviewmodel and compatability with no goals have been done

--- app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/SuccessoratorApplication.java
@@ -12,7 +12,9 @@ public class SuccessoratorApplication extends Application {
     public void onCreate(){
         super.onCreate();
         // we use the default goals here -- change later
-        this.dataSource = InMemoryDataSource.fromDefault();
+        // if we eventually go custom this is the way
+        //this.dataSource = InMemoryDataSource.fromDefault();
+        this.dataSource = InMemoryDataSource.fromDefaultEmpty(); // it works
         this.goalRepository = new GoalRepository(dataSource);
     }
     public GoalRepository getGoalRepository() {
--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/data/InMemoryDataSource.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/data/InMemoryDataSource.java
@@ -62,6 +62,8 @@ public class InMemoryDataSource {
             new Goal("Text Maria", 3, false)
 //            new Goal("This Massive Wall Of Text Goes On And On For All Eternity Or At Least Until It gets Off THe Screen In which Case You Will Stop Seeing It At All", 4)
     );
+
+
     public static InMemoryDataSource fromDefault(){
         var data = new InMemoryDataSource();
         for (Goal goal : DEFAULT_GOALS) {
@@ -69,4 +71,19 @@ public class InMemoryDataSource {
         }
         return data;
     }
+
+    // most likely we dont need this
+    public final static List<Goal> DEFAULT_EMPTY = List.of(
+    );
+    public static InMemoryDataSource fromDefaultEmpty(){
+        var data = new InMemoryDataSource();
+
+        // completely unnecessary
+        for (Goal goal : DEFAULT_EMPTY) {
+            data.putGoal(goal);
+        }
+
+        return data;
+    }
+
 }
