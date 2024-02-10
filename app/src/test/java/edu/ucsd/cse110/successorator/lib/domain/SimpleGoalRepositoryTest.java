0bbb64c55dc59e2e759f3018c75a4b82cc63abf3
Yoav Gutman <ygutman@ucsd.edu>
2024-02-10 14:44:36 -0800

    begun the arduous process of adding tests

---
+++ app/src/test/java/edu/ucsd/cse110/successorator/lib/domain/SimpleGoalRepositoryTest.java
@@ -0,0 +1,34 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import org.junit.Test;
+
+public class SimpleGoalRepositoryTest {
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void testSave() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void append() {
+    }
+
+    @Test
+    void prepend() {
+    }
+}
--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
@@ -3,6 +3,8 @@ package edu.ucsd.cse110.successorator.lib.domain;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.Objects;
+
 public class Goal {
     // we do not want null strings -- that is lame
     private final @NonNull String contents;
@@ -37,5 +39,18 @@ public class Goal {
     }
     public @NonNull Goal withComplete(@NonNull Boolean completed){
         return new Goal(this.contents, this.id, completed, this.sortOrder);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Goal goal = (Goal) o;
+        return sortOrder == goal.sortOrder && Objects.equals(contents, goal.contents) && Objects.equals(id, goal.id) && Objects.equals(completed, goal.completed);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contents, id, completed, sortOrder);
     }
 }
