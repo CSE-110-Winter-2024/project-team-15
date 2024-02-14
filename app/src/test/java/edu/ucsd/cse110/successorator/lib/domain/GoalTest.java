fbb4ea8be3bda0ac2fe989c2ad6b14e6c94c397c
emaresmoreno <emaresmoreno@ucsd.edu>
2024-02-13 17:46:23 -0800

    retry to commit to master

--- app/src/test/java/edu/ucsd/cse110/successorator/lib/domain/GoalTest.java
+++ app/src/test/java/edu/ucsd/cse110/successorator/lib/domain/GoalTest.java
@@ -71,7 +71,7 @@ public class GoalTest {
                 new Goal("Grocery shopping", 1, false, 1),
                 new Goal("Make dinner", 2, false, 2),
                 new Goal("Text Maria", 3, false, 3)
-//             new Goal("This Massive Wall Of Text Goes On And On For All Eternity Or At Least Until It gets Off THe Screen In which Case You Will Stop Seeing It At All", 4)
+//             
         );
         var dataSource = new InMemoryDataSource();
         for (Goal goal : testGoals) {
@@ -96,7 +96,7 @@ public class GoalTest {
                 new Goal("Grocery shopping", 1, false, 1),
                 new Goal("Make dinner", 2, false, 2),
                 new Goal("Text Maria", 3, false, 3)
-//             new Goal("This Massive Wall Of Text Goes On And On For All Eternity Or At Least Until It gets Off THe Screen In which Case You Will Stop Seeing It At All", 4)
+//
         );
         var dataSource = new InMemoryDataSource();
         for (Goal goal : testGoals) {
@@ -118,7 +118,7 @@ public class GoalTest {
                 new Goal("Grocery shopping", 1, false, 1),
                 new Goal("Make dinner", 2, false, 2),
                 new Goal("Text Maria", 3, true, 3)
-//             new Goal("This Massive Wall Of Text Goes On And On For All Eternity Or At Least Until It gets Off THe Screen In which Case You Will Stop Seeing It At All", 4)
+//
         );
         var dataSource = new InMemoryDataSource();
         for (Goal goal : testGoals) {
